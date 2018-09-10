0d72a531f6ce8bed8167df3cd7fcae18c9aa0e3f
JasonCrane <JasonCrane@quadax.com>
2018-09-10 15:07:30 -0400

    Completed module 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,21 +20,46 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if(grades[threshold - 1] <= averageGrade) {
+            if (grades[threshold - 1] <= averageGrade)
+            {
                 return 'A';
-            } else if (grades[(threshold * 2) - 1] <= averageGrade)
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
             {
                 return 'B';
-            } else if (grades[(threshold * 3) - 1] <= averageGrade)
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
             {
                 return 'C';
-            } else if (grades[(threshold * 4) - 1] <= averageGrade)
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
             {
                 return 'D';
-            } else
+            }
+            else
             {
                 return 'F';
             }
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Randed grading requires at least 5 students." );
+                return;
+            }
+            base.CalculateStatistics(); //call base class un-overriden method if there were over 5 students
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
